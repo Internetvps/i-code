f814d708c6a707e50ebbec939bd6ad42e78ea2f6
Internetvps <89685787+Internetvps@users.noreply.github.com>
2023-06-24 01:11:54 +0800

    Add files via upload

--- update/run-update.sh
+++ update/run-update.sh
@@ -11,7 +11,7 @@ if [ "$(systemd-detect-virt)" == "openvz" ]; then
 fi
 echo ""
 version=$(cat /home/ver)
-ver=$( curl https://raw.githubusercontent.com/${GitUser}/version-t/main/version.conf )
+ver=$( curl https://raw.githubusercontent.com/${GitUser}/i-code/main/version.conf )
 clear
 # CEK UPDATE
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
@@ -19,7 +19,7 @@ Info1="${Green_font_prefix}[$version]${Font_color_suffix}"
 Info2="${Green_font_prefix}[LATEST VERSION]${Font_color_suffix}"
 Error="Version ${Green_font_prefix}[$ver]${Font_color_suffix} available${Red_font_prefix}[Please Update]${Font_color_suffix}"
 version=$(cat /home/ver)
-new_version=$( curl https://raw.githubusercontent.com/${GitUser}/version-t/main/version.conf | grep $version )
+new_version=$( curl https://raw.githubusercontent.com/${GitUser}/i-code/main/version.conf | grep $version )
 #Status Version
 if [ $version = $new_version ]; then
 sts="${Info2}"
@@ -47,7 +47,7 @@ read -p "Please Choose 1 or x & y : " option2
 case $option2 in
 1)
 version=$(cat /home/ver)
-new_version=$( curl https://raw.githubusercontent.com/${GitUser}/version-t/main/version.conf | grep $version )
+new_version=$( curl https://raw.githubusercontent.com/${GitUser}//i-code/main/version.conf | grep $version )
 if [ $version = $new_version ]; then
 clear
 echo ""
@@ -99,7 +99,7 @@ clear
 echo -e ""
 echo -e "\e[0;32mDownloaded successfully!\e[0m"
 echo ""
-ver=$( curl https://raw.githubusercontent.com/${GitUser}/version-t/main/version.conf )
+ver=$( curl https://raw.githubusercontent.com/${GitUser}//i-code/main/version.conf )
 sleep 1
 echo -e "\e[0;32mPatching New Update, Please Wait...\e[0m"
 echo ""

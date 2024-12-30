98733728ba1c2d8a8c1da448449918ab430daaba
Internetvps <89685787+Internetvps@users.noreply.github.com>
2024-12-30 18:33:40 +0800

    Add files via upload

--- update/update.sh
+++ update/update.sh
@@ -1,43 +1,16 @@
+#!/bin/bash
 #Script Auto Reboot Vps
 #wget https://github.com/${GitUser}/
-GitUser="internetvps"
+GitUser="Internetvps"
 #IZIN SCRIPT
 MYIP=$(curl -sS ipv4.icanhazip.com)
-# Valid Script
-VALIDITY () {
-    today=`date -d "0 days" +"%Y-%m-%d"`
-    Exp1=$(curl https://raw.githubusercontent.com/${GitUser}/allow1/main/ipvps.conf | grep $MYIP | awk '{print $4}')
-    if [[ $today < $Exp1 ]]; then
-    echo -e "\e[32mYOUR SCRIPT ACTIVE..\e[0m"
-    else
-    echo -e "\e[31mYOUR SCRIPT HAS EXPIRED!\e[0m";
-    echo -e "\e[31mPlease renew your ipvps first\e[0m"
-    exit 0
-fi
-}
-IZIN=$(curl https://raw.githubusercontent.com/${GitUser}/allow1/main/ipvps.conf | awk '{print $5}' | grep $MYIP)
-if [ $MYIP = $IZIN ]; then
-echo -e "\e[32mPermission Accepted...\e[0m"
-else
-
-fi
 clear
-# LINE COLOUR
-line=$(cat /etc/line)
-# TEXT COLOUR BELOW
-below=$(cat /etc/below)
-# BACKGROUND TEXT COLOUR
-back_text=$(cat /etc/back)
-# NUMBER COLOUR
-number=$(cat /etc/number)
-# TEXT ON BOX COLOUR
-box=$(cat /etc/box)
 RED='\e[1;31m'
 GREEN='\e[0;32m'
 BLUE='\e[0;34m'
 NC='\e[0m'
 version=$(cat /home/ver)
-ver=$( curl https://raw.githubusercontent.com/${GitUser}/version/main/version.conf )
+ver=$( curl https://raw.githubusercontent.com/${GitUser}/version-t/main/version.conf )
 clear
 # CEK UPDATE
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
@@ -45,7 +18,7 @@ Info1="${Green_font_prefix}[$version]${Font_color_suffix}"
 Info2="${Green_font_prefix}[LATEST VERSION]${Font_color_suffix}"
 Error="Version ${Green_font_prefix}[$ver]${Font_color_suffix} available${Red_font_prefix}[Please Update]${Font_color_suffix}"
 version=$(cat /home/ver)
-new_version=$( curl https://raw.githubusercontent.com/${GitUser}/version/main/version.conf | grep $version )
+new_version=$( curl https://raw.githubusercontent.com/${GitUser}/version-t/main/version.conf | grep $version )
 #Status Version
 if [ $version = $new_version ]; then
 sts="${Info2}"
@@ -57,19 +30,18 @@ clear
 # Echo Shell
 echo ""
 echo ""
-echo -e "   \e[$line--------------------------------------------------------\e[m"
-echo -e "   \e[$back_text                 \e[30m[\e[$box UPDATE SCRIPT MENU\e[30m ]                 \e[m"
-echo -e "   \e[$line--------------------------------------------------------\e[m"
-echo -e "   \e[$below VERSION NOW >> $Info1"
-echo -e "   \e[$below STATUS UPDATE >> $sts"
+echo -e "  .-------------------------------------------------------." | lolcat
+echo -e "  |                    UPDATE SCRIPT MENU                 |" | lolcat
+echo -e "  '-------------------------------------------------------'" | lolcat
+echo -e "  \e[0;36mVERSION NOW \e[0;33m>>\e[0m $Info1"
+echo -e "  \e[0;36mSTATUS UPDATE \e[0;33m>>\e[0m $sts"
 echo -e ""
-echo -e "     \e[$number 1.\e[m \e[$below Change Banner SSH\e[m"
-echo -e "     \e[$number 2.\e[m \e[$below Check Update/New Version Script\e[m"
+echo -e "     \e[0;36m1.\e[0m Change Banner SSH"
+echo -e "     \e[0;36m2.\e[0m Check Update/New Version Script"
+echo ""
+echo -e "  --------------------------------------------------------" | lolcat
+echo -e "    \e[0;32m[x]\e[0m  Back To Main Menu"
 echo -e ""
-echo -e "   \e[$line--------------------------------------------------------\e[m"
-echo -e "   \e[$back_text \e[$box x)   MENU                                             \e[m"
-echo -e "   \e[$line--------------------------------------------------------\e[m"
-echo -e "\e[$line"
 read -p "     Select From Options [1-3 or x] :  " port
 echo -e ""
 case $port in
